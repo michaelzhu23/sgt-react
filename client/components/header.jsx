0713f00208dcb12a1def416f9c4e70e037709020
Michael Zhu <mzhu23@gmail.com>
2020-04-20 05:49:50 +0000

    Edited Header component to just be a function instead of a class

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
-export default class Header extends React.component {
-  render() {
-    return (
-      <h1 className="col-lg-6 col-12 text-lg-left text-center">Student Grade Table</h1>
-    );
-  }
+export default function Header() {
+  return (
+    <h1 className="col-lg-6 col-12 text-lg-left text-center">Student Grade Table</h1>
+  );
 }
