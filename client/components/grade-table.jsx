cacd63008c373ef724c68a65159facf74e92a5c7
Michael Zhu <mzhu23@gmail.com>
2020-04-20 09:11:34 +0000

    Defined GradeTable component that renders a table and uses the Grade component to render table data

--- client/components/grade-table.jsx
+++ client/components/grade-table.jsx
@@ -11,3 +11,30 @@ function Grade(props) {
     </tr>
   );
 }
+
+function GradeTable(props) {
+  return (
+    <table className="table table-striped mb-4">
+      <thead className="thead-dark">
+        <tr>
+          <th>Student Name</th>
+          <th>Course</th>
+          <th>Grade</th>
+          <th>Operations</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          props.grades.map(grade => {
+            return (
+              <Grade
+                key={grade.id}
+                grade={grade}
+              />
+            );
+          })
+        }
+      </tbody>
+    </table>
+  );
+}
