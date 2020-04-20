659b39fb4f556dfbed5cdd5b76e3fce68112af00
Michael Zhu <mzhu23@gmail.com>
2020-04-20 05:33:22 +0000

    Created and defined Header component that shows page title

---
+++ client/components/header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Header extends React.component {
+  render() {
+    return (
+      <h1 className="col-lg-6 col-12 text-lg-left text-center">Student Grade Table</h1>
+    );
+  }
+}
