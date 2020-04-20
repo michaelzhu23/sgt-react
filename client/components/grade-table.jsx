80a99a991c9723f8c9af75976a71ea8cde46d9e1
Michael Zhu <mzhu23@gmail.com>
2020-04-20 11:08:18 +0000

    Added conditional in GradeTable component to display a message if there are no grades

--- client/components/grade-table.jsx
+++ client/components/grade-table.jsx
@@ -13,27 +13,34 @@ function Grade(props) {
 }
 
 export default function GradeTable(props) {
+  let noGrades;
+  if (!props.grades.length) {
+    noGrades = <p>No Grades Recorded</p>;
+  }
   return (
-    <table className="table table-striped mb-4">
-      <thead className="thead-dark">
-        <tr>
-          <th>Student Name</th>
-          <th>Course</th>
-          <th>Grade</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          props.grades.map(grade => {
-            return (
-              <Grade
-                key={grade.id}
-                grade={grade}
-              />
-            );
-          })
-        }
-      </tbody>
-    </table>
+    <>
+      <table className="table table-striped mb-4">
+        <thead className="thead-dark">
+          <tr>
+            <th>Student Name</th>
+            <th>Course</th>
+            <th>Grade</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            props.grades.map(grade => {
+              return (
+                <Grade
+                  key={grade.id}
+                  grade={grade}
+                />
+              );
+            })
+          }
+        </tbody>
+      </table>
+      {noGrades}
+    </>
   );
 }
